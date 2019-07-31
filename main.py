27dbbd4642f930ae675e1300b8bdc86976887f8f
Oliver Rogers <oliver.rogers101@gmail.com>
2019-07-31 14:11:43 +0100

    added swagger route to api

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from flask import Flask
+from flask_swagger_ui import get_swaggerui_blueprint
+
 from controls import SystemControls
+
 import os
 
 app = Flask(__name__)
 system = SystemControls()
+
 
 @app.route('/')
 def index():
     hostname = system.getHostname()
     return('This API controls the media server running on {}.'.format(hostname))
 
+
 @app.route('/hostname')
 def hostname():
     return system.getHostname()
+
 
 @app.route('/reboot')
 @app.route('/restart')
@@ -22,6 +28,21 @@ def restart():
         return('Success')
     except Exception as e:
         return('Failed')
+
+
+
+### swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Seans-Python-Flask-REST-Boilerplate"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
 
 
 if  __name__ == '__main__':
