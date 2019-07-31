00fac14379b729d5960ef60822190a7b149682de
Oliver Rogers <oliver.rogers101@gmail.com>
2019-07-31 14:38:07 +0100

    started work on process check

--- controls.py
+++ controls.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+
 class SystemControls:
     def __init__(self):
         self.hostname = subprocess.check_output(['hostname'])
@@ -7,6 +8,14 @@ class SystemControls:
     def getHostname(self):
         return self.hostname
 
+    def checkProcess(self,process_name):
+        command = 'ps -ef | grep {}'.format(process_name)
+        ps_out = subprocess.check_output(['ps', '-ef', '|', 'grep','hi'])
+        print(ps_out)
+ 
     def reboot(self):
         os.system('reboot')
 
+
+#sc = SystemControls()
+#sc.checkProcess('deluge')
