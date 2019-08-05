7d3f9a1e8b4e0bf8b144159aa8be9cca0f2c74fd
Oliver Rogers <oliver.rogers101@gmail.com>
2019-08-05 11:50:06 +0100

    changed process methon

--- controls.py
+++ controls.py
@@ -10,12 +10,15 @@ class SystemControls:
 
     def checkProcess(self,process_name):
         command = 'ps -ef | grep {}'.format(process_name)
-        ps_out = subprocess.check_output(['ps', '-ef', '|', 'grep','hi'])
-        print(ps_out)
+        ps_out = subprocess.Popen(command,
+                                  shell=True,
+                                  stdout=subprocess.PIPE)
+        stout, _ = ps_out.communicate()
+        print(stout)
  
     def reboot(self):
         os.system('reboot')
 
 
-#sc = SystemControls()
-#sc.checkProcess('deluge')
+sc = SystemControls()
+sc.checkProcess('deluge')
