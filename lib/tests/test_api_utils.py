b83f6de97ee38825c84beeaabb543cfdb29c23ff
Oliver Rogers <oliver.rogers101@gmail.com>
2019-08-07 12:27:50 +0100

    added test

--- lib/tests/test_api_utils.py
+++ lib/tests/test_api_utils.py
@@ -26,5 +26,16 @@ class TestController(unittest.TestCase):
         target = (json.dumps({'status': 'success', 'data': data}),200)
         self.assertEqual(res, target)
 
+    def test_sendSuccess01(self):
+        try:
+            data = 'this is a unit test'
+            res =  self.utils.sendSuccess(data)
+        except:
+            assert False
+        target_dict = {'status': 'success',
+                       'text': data}
+        target = (json.dumps(target_dict),200)
+        self.assertEqual(res, target)
+
 if __name__ == 'main':
     unittest.main()
