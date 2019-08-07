c6a88b9d2aa839fd9e5e03db44d785edc0c84929
Oliver Rogers <oliver.rogers101@gmail.com>
2019-08-07 11:38:30 +0100

    added api utils file

---
+++ lib/api_utils.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+__author__ = "Oliver Rogers"
+__version__ = "1.0.0"
+__maintainer__ = "Oliver Rogers"
+__email__ = "oliver.rogers101@gmail.com"
+__status__ = "Development"
+
+
