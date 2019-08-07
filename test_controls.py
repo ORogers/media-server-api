b07f4bf2b620f884e3e14f573a0a7af3589273de
Oliver Rogers <oliver.rogers101@gmail.com>
2019-08-07 01:17:34 +0100

    fixed typo

--- test_controls.py
+++ test_controls.py
@@ -4,7 +4,7 @@ from controls import SystemControls
 class TestController(unittest.TestCase):
 
     def setUp(self):
-        self.system = SystemControls()
+        self.system = SystemControls('test')
 
     def test_getHostname(self):
         try:
@@ -13,28 +13,26 @@ class TestController(unittest.TestCase):
         except:
             assert False
 
-    def test_checkProgress(self):
+    def test_checkProcess(self):
         try:
            self.system.checkProcess('deluge')
            assert True
         except:
            assert False
 
-    def test_checkProgress01(self):
+    def test_checkProcess01(self):
         try:
            self.system.checkProcess('deluge-web')
            assert True
         except:
            assert False
 
-    def test_checkProgress02(self):
+    def test_checkProcess02(self):
         try:
            self.system.checkProcess('Plex Media Server')
            assert True
         except:
            assert False
-
-
 
 
 if __name__ == 'main':
