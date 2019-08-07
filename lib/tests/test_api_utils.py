903c5513205d26c1ef4af227cf63ddd49f4030d7
Oliver Rogers <oliver.rogers101@gmail.com>
2019-08-07 14:33:38 +0100

    added tests

--- lib/tests/test_api_utils.py
+++ lib/tests/test_api_utils.py
@@ -4,7 +4,7 @@ sys.path.insert(0,'../')
 import unittest
 from api_utils import APIUtils
 
-class TestController(unittest.TestCase):
+class TestAPIUtils(unittest.TestCase):
 
     def setUp(self):
         self.utils = APIUtils('test')
@@ -12,7 +12,8 @@ class TestController(unittest.TestCase):
     def test_sendSuccess(self):
         try:
             res =  self.utils.sendSuccess()
-        except:
+        except Exception as e:
+            print(e)
             assert False
         target = (json.dumps({'status': 'success'}),200)
         self.assertEqual(res, target)
@@ -26,7 +27,7 @@ class TestController(unittest.TestCase):
         target = (json.dumps({'status': 'success', 'data': data}),200)
         self.assertEqual(res, target)
 
-    def test_sendSuccess01(self):
+    def test_sendSuccess02(self):
         try:
             data = 'this is a unit test'
             res =  self.utils.sendSuccess(data)
@@ -36,6 +37,35 @@ class TestController(unittest.TestCase):
                        'text': data}
         target = (json.dumps(target_dict),200)
         self.assertEqual(res, target)
+
+    def test_sendFailure(self):
+        try:
+            res =  self.utils.sendFailure(None,404)
+        except:
+            assert False
+        target = (json.dumps({'status': 'failure'}),404)
+        self.assertEqual(res, target)
+
+    def test_sendFailure01(self):
+        try:
+            data = {'test': 2}
+            res =  self.utils.sendFailure(data)
+        except:
+            assert False
+        target = (json.dumps({'status': 'failure', 'data': data}),500)
+        self.assertEqual(res, target)
+
+    def test_sendFailure02(self):
+        try:
+            data = 'this is a unit test'
+            res =  self.utils.sendFailure(data)
+        except:
+            assert False
+        target_dict = {'status': 'failure',
+                       'text': data}
+        target = (json.dumps(target_dict),500)
+        self.assertEqual(res, target)
+   
 
 if __name__ == 'main':
     unittest.main()
